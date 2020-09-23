fc2e15bb78cdb7ca57110e0be1a31103d43d84a8
Carlos A. Guevara <solrak.m@gmail.com>
2020-09-23 09:56:39 -0500

    trait to add uuid

---
+++ app/Models/Traits/HasUuid.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace App\Models\Traits;
+
+
+use Illuminate\Support\Str;
+
+trait HasUuid
+{
+
+    public function getIncrementing()
+    {
+        return false;
+    }
+
+    protected static function bootHasUuid()
+    {
+        static::creating(function ($model) {
+            $model->{$model->getKeyName()} = Str::uuid()->toString();
+        });
+    }
+}
